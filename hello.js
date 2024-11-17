55da4e5f0e8b6591882a077a6c97843d9fe94c2b
ahmed <96777623+Ah22920@users.noreply.github.com>
2024-11-17 15:59:49 +0300

    Revert "add github console.log to hello.js"
    This reverts commit 5d1cce9c4c3f33a50b79e5c4cd556629a7e81474.

--- hello.js
+++ hello.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 console.log("hello world");
 console.log("Hello,github");
 console.log("Hello from dev-adrian");
 console.log('this is bad code!');
+=======
+console.log("hello world") 
+>>>>>>> parent of 5d1cce9 (add github console.log to hello.js)
